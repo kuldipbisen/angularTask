314747bfd15d14137b494b5a045e144be795077f
Kuldipkumar Radhelal Bisen <kuldipkumarradhelal_bisen@epam.com>
2026-05-21 18:31:24 +0530

    Add new features to task1 branch

---
+++ src/app/task-one/task-one.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-one',
+  imports: [],
+  templateUrl: './task-one.component.html',
+  styleUrl: './task-one.component.scss'
+})
+export class TaskOneComponent {
+
+}
